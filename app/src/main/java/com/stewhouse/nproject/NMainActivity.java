d31bff21a20b71ce320298a1e7f612d8d35cb62c
Alphys <je.park@memebox.com>
2016-07-08 20:56:56 +0900

    1. Initial commit. 2. Apply Proguard.

---
+++ app/src/main/java/com/stewhouse/nproject/NMainActivity.java
@@ -0,0 +1,13 @@
+package com.stewhouse.nproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
