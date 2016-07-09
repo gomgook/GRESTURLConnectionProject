c7baa1086c272a3bfde609da5c9a54d56e27f5ff
Alphys <je.park@memebox.com>
2016-07-09 20:51:03 +0900

    Code refactoring. 	- Change connectionParams type to HashMap to increase readability.

--- app/src/main/java/com/stewhouse/nproject/utility/GRESTURLConnection.java
+++ app/src/main/java/com/stewhouse/nproject/utility/GRESTURLConnection.java
@@ -6,7 +6,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
+import java.net.URLConnection;
+import java.util.HashMap;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -14,6 +15,11 @@ import javax.net.ssl.HttpsURLConnection;
  * Created by Gomguk on 16. 7. 8..
  */
 public class GRESTURLConnection {
+    private final static String CONNECTION_PARAM_URL = "url";
+    private final static String CONNECTION_PARAM_TIMEOUT = "timeout";
+    private final static String CONNECTION_PARAM_REQUEST_TYPE = "request_type";
+    private final static String CONNECTION_PARAM_HEADERS = "headers";
+
     public enum SchemeType {
         HTTP, HTTPS
     }
@@ -22,39 +28,49 @@ public class GRESTURLConnection {
         GET, POST, PUT, DELETE
     }
 
-    private AsyncTask<Object, Object, Object> mAsyncTask = null;
+    private AsyncTask<HashMap, Object, Object> mAsyncTask = null;
 
-    public static void execute(String url, int timeOut, RequestType requestType) {
+    public static void execute(String url, int timeOut, RequestType requestType, HashMap headers) {
         GRESTURLConnection urlConnection = new GRESTURLConnection();
-        ArrayList<Object> connectionParams = new ArrayList<>();
+        HashMap<String, Object> connectionParams = new HashMap<>();
 
-        connectionParams.add(url);
-        connectionParams.add(timeOut);
-        connectionParams.add(requestType);
+        connectionParams.put(CONNECTION_PARAM_URL, url);
+        connectionParams.put(CONNECTION_PARAM_TIMEOUT, timeOut);
+        connectionParams.put(CONNECTION_PARAM_REQUEST_TYPE, requestType);
+        connectionParams.put(CONNECTION_PARAM_HEADERS, headers);
 
         urlConnection.executeInternal(connectionParams);
     }
 
-    private void executeInternal(ArrayList<Object> connectionParams) {
-        mAsyncTask = new AsyncTask<Object, Object, Object>() {
+    private void executeInternal(HashMap connectionParams) {
+        mAsyncTask = new AsyncTask<HashMap, Object, Object>() {
 
             @Override
-            protected Object doInBackground(Object... params) {
-                String urlStr = (String) params[0];
-                int timeOut = (int) params[1];
-                RequestType requestType = (RequestType) params[2];
+            protected Object doInBackground(HashMap... params) {
+                HashMap<String, Object> requestParams = (HashMap) params[0];
+
+                String urlStr = (String) requestParams.get(CONNECTION_PARAM_URL);
+                int timeOut = (int) requestParams.get(CONNECTION_PARAM_TIMEOUT);
+                RequestType requestType = (RequestType) requestParams.get(CONNECTION_PARAM_REQUEST_TYPE);
 
                 StringBuilder stringBuilder = new StringBuilder();
 
                 try {
                     URL url = new URL(urlStr);
+                    URLConnection conn = url.openConnection();
 
                     if (timeOut > -1) {
+
+                        // Set Integrated parameters.
+                        conn.setConnectTimeout(timeOut);
+                        if (requestParams.get(CONNECTION_PARAM_HEADERS) != null) {
+                            setRequestHeader(conn, (HashMap<String, String>) requestParams.get(CONNECTION_PARAM_HEADERS));
+                        }
+
                         if (checkScheme(urlStr).equals(SchemeType.HTTP)) {
-                            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+                            HttpURLConnection httpConn = (HttpURLConnection) conn;
 
                             httpConn.setRequestMethod(requestType.toString());
-                            httpConn.setConnectTimeout(timeOut);
 
                             if (httpConn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
                                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
@@ -73,10 +89,9 @@ public class GRESTURLConnection {
                             }
 
                         } else if (checkScheme(urlStr).equals(SchemeType.HTTPS)) {
-                            HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
+                            HttpsURLConnection httpsConn = (HttpsURLConnection) conn;
 
                             httpsConn.setRequestMethod(requestType.toString());
-                            httpsConn.setConnectTimeout(timeOut);
 
                             if (httpsConn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
                                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsConn.getInputStream()));
@@ -117,5 +132,11 @@ public class GRESTURLConnection {
         }
 
         return null;
+    }
+
+    private void setRequestHeader(URLConnection conn, HashMap<String, String> headers) {
+        for (String key : headers.keySet()) {
+            conn.addRequestProperty(key, headers.get(key));
+        }
     }
 }
