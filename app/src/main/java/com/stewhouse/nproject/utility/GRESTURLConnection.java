7aee28a6ea17862ea6a2675783aa7aa5c0b0efc9
gomgook <kaioga4187@gmail.com>
2016-07-10 20:16:26 +0900

    Change the Connection class to extend AsyncTask.

--- app/src/main/java/com/stewhouse/nproject/utility/GRESTURLConnection.java
+++ app/src/main/java/com/stewhouse/nproject/utility/GRESTURLConnection.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
@@ -14,11 +15,18 @@ import javax.net.ssl.HttpsURLConnection;
 /**
  * Created by Gomguk on 16. 7. 8..
  */
-public class GRESTURLConnection {
+public class GRESTURLConnection extends AsyncTask<HashMap, Object, Object> {
+
+    public interface GRESTURLConnectionListener {
+        void onPostExecute(Object result);
+    }
+
     private final static String CONNECTION_PARAM_URL = "url";
     private final static String CONNECTION_PARAM_TIMEOUT = "timeout";
     private final static String CONNECTION_PARAM_REQUEST_TYPE = "request_type";
     private final static String CONNECTION_PARAM_HEADERS = "headers";
+    private final static String CONNECTION_PARAM_REQUEST_BODY = "request_body";
+    private final static String CONNECTION_PARAM_REQUEST_BODY_TYPE = "request_body_type";
 
     public enum SchemeType {
         HTTP, HTTPS
@@ -28,110 +36,129 @@ public class GRESTURLConnection {
         GET, POST, PUT, DELETE
     }
 
-    private AsyncTask<HashMap, Object, Object> mAsyncTask = null;
+    private GRESTURLConnectionListener mListener = null;
 
-    public static void execute(String url, int timeOut, RequestType requestType, HashMap headers) {
-        GRESTURLConnection urlConnection = new GRESTURLConnection();
+    public void setListener(GRESTURLConnectionListener listener) {
+        mListener = listener;
+    }
+
+    public void execute(String url, int timeOut, RequestType requestType, HashMap<String, String> headers, String requestBody, String requestBodyType) {
         HashMap<String, Object> connectionParams = new HashMap<>();
 
         connectionParams.put(CONNECTION_PARAM_URL, url);
         connectionParams.put(CONNECTION_PARAM_TIMEOUT, timeOut);
         connectionParams.put(CONNECTION_PARAM_REQUEST_TYPE, requestType);
         connectionParams.put(CONNECTION_PARAM_HEADERS, headers);
+        connectionParams.put(CONNECTION_PARAM_REQUEST_BODY, requestBody);
+        connectionParams.put(CONNECTION_PARAM_REQUEST_BODY_TYPE, requestBodyType);
 
-        urlConnection.executeInternal(connectionParams);
+        this.execute(connectionParams);
     }
 
-    private void executeInternal(HashMap connectionParams) {
-        mAsyncTask = new AsyncTask<HashMap, Object, Object>() {
+    @Override
+    protected Object doInBackground(HashMap... params) {
+        HashMap<String, Object> requestParams = (HashMap) params[0];
 
-            @Override
-            protected Object doInBackground(HashMap... params) {
-                HashMap<String, Object> requestParams = (HashMap) params[0];
+        String urlStr = (String) requestParams.get(CONNECTION_PARAM_URL);
+        int timeOut = (int) requestParams.get(CONNECTION_PARAM_TIMEOUT);
+        RequestType requestType = (RequestType) requestParams.get(CONNECTION_PARAM_REQUEST_TYPE);
 
-                String urlStr = (String) requestParams.get(CONNECTION_PARAM_URL);
-                int timeOut = (int) requestParams.get(CONNECTION_PARAM_TIMEOUT);
-                RequestType requestType = (RequestType) requestParams.get(CONNECTION_PARAM_REQUEST_TYPE);
+        StringBuilder stringBuilder = new StringBuilder();
 
-                StringBuilder stringBuilder = new StringBuilder();
+        try {
+            URL url = new URL(urlStr);
+            URLConnection conn = url.openConnection();
 
-                try {
-                    URL url = new URL(urlStr);
-                    URLConnection conn = url.openConnection();
+            if (timeOut > -1) {
 
-                    if (timeOut > -1) {
+                // Set Integrated parameters.
+                conn.setConnectTimeout(timeOut);
 
-                        // Set Integrated parameters.
-                        conn.setConnectTimeout(timeOut);
-                        if (requestParams.get(CONNECTION_PARAM_HEADERS) != null) {
-                            setRequestHeader(conn, (HashMap<String, String>) requestParams.get(CONNECTION_PARAM_HEADERS));
-                        }
+                if (requestParams.get(CONNECTION_PARAM_HEADERS) != null) {
+                    setRequestHeader(conn, (HashMap<String, String>) requestParams.get(CONNECTION_PARAM_HEADERS));
+                }
 
-                        if (checkScheme(urlStr).equals(SchemeType.HTTP)) {
-                            HttpURLConnection httpConn = (HttpURLConnection) conn;
+                if (requestParams.get(CONNECTION_PARAM_REQUEST_BODY) != null && requestParams.get(CONNECTION_PARAM_REQUEST_BODY_TYPE) != null) {
+                    String requestBody = (String) requestParams.get(CONNECTION_PARAM_REQUEST_BODY);
+                    String requestBodyType = (String) requestParams.get(CONNECTION_PARAM_REQUEST_BODY_TYPE);
 
-                            httpConn.setRequestMethod(requestType.toString());
+                    conn.addRequestProperty("content_type", requestBodyType);
+                    conn.setDoOutput(true);
+                    OutputStream outputStream = conn.getOutputStream();
+                    outputStream.write(requestBody.getBytes("UTF-8"));
+                    outputStream.close();
+                }
 
-                            if (httpConn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
-                                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
-                                String result;
+                // Set parameters which is classified by whether the connection is HTTP or HTTPS.
+                if (checkScheme(urlStr).equals(SchemeType.HTTP)) {
+                    HttpURLConnection httpConn = (HttpURLConnection) conn;
 
-                                while (true) {
-                                    result = bufferedReader.readLine();
+                    httpConn.setRequestMethod(requestType.toString());
 
-                                    if (result == null) {
-                                        break;
-                                    }
-                                    stringBuilder.append(result + "\n");
-                                }
-                                bufferedReader.close();
-                                httpConn.disconnect();
-                            } else {
+                    if (httpConn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+                        String result;
 
-                                // TODO: Return Error with response code and response message.
-                                return null;
+                        while (true) {
+                            result = bufferedReader.readLine();
+
+                            if (result == null) {
+                                break;
                             }
-
-                        } else if (checkScheme(urlStr).equals(SchemeType.HTTPS)) {
-                            HttpsURLConnection httpsConn = (HttpsURLConnection) conn;
-
-                            httpsConn.setRequestMethod(requestType.toString());
-
-                            if (httpsConn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
-                                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsConn.getInputStream()));
-                                String result;
-
-                                while (true) {
-                                    result = bufferedReader.readLine();
-
-                                    if (result == null) {
-                                        break;
-                                    }
-                                    stringBuilder.append(result + "\n");
-                                }
-                                bufferedReader.close();
-                                httpsConn.disconnect();
-                            } else {
-
-                                // TODO: Return Error with response code and response message.
-                                return null;
-                            }
-
-                        } else {    // If the connection type is neither http nor https.
-                            return null;
+                            stringBuilder.append(result + "\n");
                         }
+                        bufferedReader.close();
+                        httpConn.disconnect();
                     } else {
+
+                        // TODO: Return Error with response code and response message.
                         return null;
                     }
 
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
+                } else if (checkScheme(urlStr).equals(SchemeType.HTTPS)) {
+                    HttpsURLConnection httpsConn = (HttpsURLConnection) conn;
 
-                return stringBuilder.toString();
+                    httpsConn.setRequestMethod(requestType.toString());
+
+                    if (httpsConn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsConn.getInputStream()));
+                        String result;
+
+                        while (true) {
+                            result = bufferedReader.readLine();
+
+                            if (result == null) {
+                                break;
+                            }
+                            stringBuilder.append(result + "\n");
+                        }
+                        bufferedReader.close();
+                        httpsConn.disconnect();
+                    } else {
+
+                        // TODO: Return Error with response code and response message.
+                        return null;
+                    }
+
+                } else {    // If the connection type is neither http nor https.
+                    return null;
+                }
+            } else {
+                return null;
             }
-        };
-        mAsyncTask.execute(connectionParams);
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+
+        mListener.onPostExecute(o);
     }
 
     private SchemeType checkScheme(String url) {
