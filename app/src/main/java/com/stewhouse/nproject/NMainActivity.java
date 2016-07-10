0ae66311d5aff137db11d518afaec5ebd9238cdf
gomgook <kaioga4187@gmail.com>
2016-07-11 00:13:35 +0900

    1. Change lib package to lib project. 2. Add gradle script making jar library file.

--- app/src/main/java/com/stewhouse/nproject/NMainActivity.java
+++ app/src/main/java/com/stewhouse/nproject/NMainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.stewhouse.nproject.utility.GRESTURLConnection;
+import com.stewhouse.gresturlconnection.GRESTURLConnection;
 
 public class NMainActivity extends AppCompatActivity implements GRESTURLConnection.GRESTURLConnectionListener {
 
