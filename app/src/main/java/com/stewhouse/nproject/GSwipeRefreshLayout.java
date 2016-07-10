0b9be3b93a4d030b98bbc21d6e7b632b373ea570
gomgook <kaioga4187@gmail.com>
2016-07-11 03:03:31 +0900

    Add custom SwipeRefreshLayout

---
+++ app/src/main/java/com/stewhouse/nproject/GSwipeRefreshLayout.java
@@ -0,0 +1,15 @@
+package com.stewhouse.nproject;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+
+/**
+ * Created by Gomguk on 2016-07-11.
+ */
+public class GSwipeRefreshLayout extends SwipeRefreshLayout {
+
+    public GSwipeRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
