b9054af1a2a4a5aac4a47a242f3a0ffde31c7226
Alphys <je.park@memebox.com>
2016-07-11 11:09:50 +0900

    1. Add function to set params. 2. Modify gradle script putting the .jar file to main project automatically. 3. Add custom BaseAdapter.

--- app/src/main/java/com/stewhouse/nproject/GSwipeRefreshLayout.java
+++ app/src/main/java/com/stewhouse/nproject/GSwipeRefreshLayout.java
@@ -3,13 +3,24 @@ package com.stewhouse.nproject;
 import android.content.Context;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
+import android.widget.ListView;
 
 /**
  * Created by Gomguk on 2016-07-11.
  */
 public class GSwipeRefreshLayout extends SwipeRefreshLayout {
+    private ListView mListView = null;
+    private NBaseAdapter mAdapter = null;
 
     public GSwipeRefreshLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    public void setListView(ListView listView) {
+        mListView = listView;
+    }
+
+    public void setAdapter(NBaseAdapter adapter) {
+        mAdapter = adapter;
     }
 }
