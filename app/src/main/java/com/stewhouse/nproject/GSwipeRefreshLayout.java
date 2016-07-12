1b26b29175fe60797158956c8e1e5a3fe7a4a61f
Alphys <je.park@memebox.com>
2016-07-12 15:02:09 +0900

    Code refactoring.

--- app/src/main/java/com/stewhouse/nproject/GSwipeRefreshLayout.java
+++ app/src/main/java/com/stewhouse/nproject/GSwipeRefreshLayout.java
@@ -3,6 +3,7 @@ package com.stewhouse.nproject;
 import android.content.Context;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ListView;
 
 /**
@@ -12,6 +13,8 @@ public class GSwipeRefreshLayout extends SwipeRefreshLayout {
 
     private ListView mListView = null;
     private NBaseAdapter mListAdapter = null;
+
+    private View mFooterLoadingView = null;
 
     public GSwipeRefreshLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -31,5 +34,24 @@ public class GSwipeRefreshLayout extends SwipeRefreshLayout {
 
     public void setAdapter(NBaseAdapter adapter) {
         mListAdapter = adapter;
+    }
+
+    public void setFooterLoadingView(View footerLoadingView) {
+        mFooterLoadingView = footerLoadingView;
+    }
+
+    public void addLoadingFooter() {
+        if (mListView != null && mFooterLoadingView != null) {
+            removeLoadingFooter();
+            mListView.addFooterView(mFooterLoadingView);
+        }
+    }
+
+    public void removeLoadingFooter() {
+        if (mListView != null && mFooterLoadingView != null) {
+            while (mListView.getFooterViewsCount() != 0) {
+                mListView.removeFooterView(mFooterLoadingView);
+            }
+        }
     }
 }
