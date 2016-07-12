d1ce3a1090e22d9ce80b95b115c32a894b1c94f6
Alphys <je.park@memebox.com>
2016-07-12 12:07:17 +0900

    1. Modify Exception logs. 2. Modify paging logic because of API policy.

--- gresturlconnection/src/main/java/com/stewhouse/gresturlconnection/GException.java
+++ gresturlconnection/src/main/java/com/stewhouse/gresturlconnection/GException.java
@@ -7,21 +7,28 @@ import android.util.Log;
  */
 public class GException extends Exception {
     public enum ErrorType {
-        RESPONSE_CODE_ERROR, SCHEME_NOT_SUPPORTED, REQUEST_TYPE_NOT_SUPPORTED, TIMEOUT_VALUE_INVALID, LISTENER_NULL_POINTER
+        RESPONSE_CODE_ERROR, SCHEME_NOT_SUPPORTED, REQUEST_TYPE_NOT_SUPPORTED, TIMEOUT_VALUE_INVALID, LISTENER_NULL_POINTER, UNKNOWN_ERROR
     }
 
     public GException(ErrorType errorType) {
         switch (errorType) {
-            case RESPONSE_CODE_ERROR:
+            case LISTENER_NULL_POINTER:
                 Log.e(errorType.toString(), "GRESTURLConnectionListener is null.\nGRESTURLConnectionListener should be allocated before GRESTURLConnection is used.");
+                break;
             case SCHEME_NOT_SUPPORTED:
                 Log.e(errorType.toString(), "Not supported scheme. We support HTTP or HTTPS only.");
+                break;
             case REQUEST_TYPE_NOT_SUPPORTED:
                 Log.e(errorType.toString(), "Not supported REST request type. We support GET, POST, PUT, DELETE only.");
-            case TIMEOUT_VALUE_INVALID:
+                break;
+            case RESPONSE_CODE_ERROR:
                 Log.e(errorType.toString(), "The connection response code is not 200.");
-            case LISTENER_NULL_POINTER:
+                break;
+            case TIMEOUT_VALUE_INVALID:
                 Log.e(errorType.toString(), "The timeout value is not invalid. The timeout value should be over 0.");
+                break;
+            default:
+                Log.e(ErrorType.UNKNOWN_ERROR.toString(), "Unknown error occured.");
         }
     }
 }
