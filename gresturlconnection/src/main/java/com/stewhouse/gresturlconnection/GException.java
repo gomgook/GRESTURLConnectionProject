a1a4b15654b094fe3076f5643560a3d228f41ef8
Alphys <je.park@memebox.com>
2016-07-12 14:33:26 +0900

    Modify custom Exception and exception throwing logic.

--- gresturlconnection/src/main/java/com/stewhouse/gresturlconnection/GException.java
+++ gresturlconnection/src/main/java/com/stewhouse/gresturlconnection/GException.java
@@ -1,34 +1,41 @@
 package com.stewhouse.gresturlconnection;
 
-import android.util.Log;
-
 /**
  * Created by Gomguk on 2016-07-10.
  */
 public class GException extends Exception {
+    ErrorType mErrorType = null;
+
     public enum ErrorType {
         RESPONSE_CODE_ERROR, SCHEME_NOT_SUPPORTED, REQUEST_TYPE_NOT_SUPPORTED, TIMEOUT_VALUE_INVALID, LISTENER_NULL_POINTER, UNKNOWN_ERROR
     }
 
-    public GException(ErrorType errorType) {
+    public static GException makeGException(ErrorType errorType) {
+        Throwable t = new Throwable(getErrorMessage(errorType));
+
+        return new GException(errorType, t);
+    }
+
+    public GException(ErrorType errorType, Throwable throwable) {
+        super(errorType.toString(), throwable);
+
+        mErrorType = errorType;
+    }
+
+    private static String getErrorMessage(ErrorType errorType) {
         switch (errorType) {
             case LISTENER_NULL_POINTER:
-                Log.e(errorType.toString(), "GRESTURLConnectionListener is null.\nGRESTURLConnectionListener should be allocated before GRESTURLConnection is used.");
-                break;
+                return "GRESTURLConnectionListener is null.\nGRESTURLConnectionListener should be allocated before GRESTURLConnection is used.";
             case SCHEME_NOT_SUPPORTED:
-                Log.e(errorType.toString(), "Not supported scheme. We support HTTP or HTTPS only.");
-                break;
+                return "Not supported scheme. We support HTTP or HTTPS only.";
             case REQUEST_TYPE_NOT_SUPPORTED:
-                Log.e(errorType.toString(), "Not supported REST request type. We support GET, POST, PUT, DELETE only.");
-                break;
+                return "Not supported REST request type. We support GET, POST, PUT, DELETE only.";
             case RESPONSE_CODE_ERROR:
-                Log.e(errorType.toString(), "The connection response code is not 200.");
-                break;
+                return "The connection response code is not 200.";
             case TIMEOUT_VALUE_INVALID:
-                Log.e(errorType.toString(), "The timeout value is not invalid. The timeout value should be over 0.");
-                break;
+                return "The timeout value is not invalid. The timeout value should be over 0.";
             default:
-                Log.e(ErrorType.UNKNOWN_ERROR.toString(), "Unknown error occured.");
+                return "Unknown error occurred.";
         }
     }
 }
