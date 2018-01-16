566276169781902e600ceb25679bafa2d10bb817
Alphys <je.park@memebox.com>
2018-01-16 18:48:53 +0900

    Changing ListView to RecyclerView.

--- app/src/main/java/com/stewhouse/nproject/util/GSwipeRefreshLayout.java
+++ app/src/main/java/com/stewhouse/nproject/util/GSwipeRefreshLayout.java
@@ -2,19 +2,20 @@ package com.stewhouse.nproject.util;
 
 import android.content.Context;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ListView;
 
 import com.stewhouse.nproject.NBaseResultAdapter;
+import com.stewhouse.nproject.NewNBaseResultAdapter;
 
 /**
  * Created by Gomguk on 2016-07-11.
  */
 public class GSwipeRefreshLayout extends SwipeRefreshLayout {
-
-    private ListView mListView = null;
-    private NBaseResultAdapter mListAdapter = null;
+    private RecyclerView mListView = null;
+    private NewNBaseResultAdapter mListAdapter = null;
 
     private View mFooterLoadingView = null;
 
@@ -22,20 +23,25 @@ public class GSwipeRefreshLayout extends SwipeRefreshLayout {
         super(context, attrs);
     }
 
-    public ListView getListView() {
+    public RecyclerView getListView() {
         return mListView;
     }
 
-    public void setListView(ListView listView) {
+    public void setListView(RecyclerView listView) {
         mListView = listView;
     }
 
-    public NBaseResultAdapter getAdapter() {
+    public NewNBaseResultAdapter getAdapter() {
         return mListAdapter;
     }
 
-    public void setAdapter(NBaseResultAdapter adapter) {
+    public void setAdapter(NewNBaseResultAdapter adapter) {
         mListAdapter = adapter;
+        mListView.setAdapter(mListAdapter);
+    }
+
+    public void setLayoutManager(RecyclerView.LayoutManager layoutManager) {
+        mListView.setLayoutManager(layoutManager);
     }
 
     public void setFooterLoadingView(View footerLoadingView) {
@@ -43,17 +49,19 @@ public class GSwipeRefreshLayout extends SwipeRefreshLayout {
     }
 
     public void addLoadingFooter() {
-        if (mListView != null && mFooterLoadingView != null) {
-            removeLoadingFooter();
-            mListView.addFooterView(mFooterLoadingView);
-        }
+        // TODO
+//        if (mListView != null && mFooterLoadingView != null) {
+//            removeLoadingFooter();
+//            mListView.addFooterView(mFooterLoadingView);
+//        }
     }
 
     public void removeLoadingFooter() {
-        if (mListView != null && mFooterLoadingView != null) {
-            while (mListView.getFooterViewsCount() != 0) {
-                mListView.removeFooterView(mFooterLoadingView);
-            }
-        }
+        // TODO
+//        if (mListView != null && mFooterLoadingView != null) {
+//            while (mListView.getFooterViewsCount() != 0) {
+//                mListView.removeFooterView(mFooterLoadingView);
+//            }
+//        }
     }
 }
